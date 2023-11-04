6fa8aa16c6272c8c9d3252cd45a527d8e2bc6ee1
Ahmad Alobaid <ahmad88me@gmail.com>
2023-11-04 22:38:51 +0300

    fix the new code

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import pandas as pd
 
 
 def val_to_min(val):
-    mil = int(val) - val
+    mil = val - int(val)
     seconds = int(val)%60
     minutes = int(int(val)/60)
-    "12m 1s 321ms"
     return f"{minutes}m {seconds}s {mil}ms"
 
 
@@ -14,15 +13,14 @@ def add_column(df):
     cname = "Display Time"
     df[cname] = ""
     for idx, row in df.iterrows():
-        row[cname] = val_to_min(row["time"])
-        df.at[idx, cname] = val_to_min(row["time"])
+        df.at[idx, cname] = val_to_min(row["TIME"])
     df.to_csv("new_data.txt", sep="|", index=False)
 
 
 def workflow(data_path):
     df = pd.read_csv(data_path, sep="|")
     add_column(df)
-    print(f"The new generated file is new_data.csv")
+    print(f"The new generated file is new_data.txt")
 
 
 if len(sys.argv) == 2:
